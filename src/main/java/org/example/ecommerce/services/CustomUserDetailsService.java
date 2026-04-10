611169133d072971b94189aaacc43ec01ae466bb
lyanbrito <lyanbrito613@gmail.com>
2026-04-10 11:04:56 -0300

    feat: added security config

---
+++ src/main/java/org/example/ecommerce/services/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package org.example.ecommerce.services;
+
+import org.example.ecommerce.entities.UserEntity;
+import org.example.ecommerce.repositories.UsersRepo;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepo userRepo;
+
+    public CustomUserDetailsService(UsersRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepo.findByEmail(email);
+        return User.builder()
+                .username(user.getName())
+                .password(user.getPassword())
+                .roles(user.getRoles().name().replace("ROLE_",""))
+                .build();
+    }
+}
