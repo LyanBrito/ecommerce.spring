72cebfb8a80878e7672c938a51922debcc42a725
lyanbrito <lyanbrito613@gmail.com>
2026-04-10 11:05:17 -0300

    feat: added findByEmail method

--- src/main/java/org/example/ecommerce/repositories/UsersRepo.java
+++ src/main/java/org/example/ecommerce/repositories/UsersRepo.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UsersRepo extends JpaRepository<UserEntity, UUID> {
+    UserEntity findByEmail(String email);
+    String email(String email);
+
 }
